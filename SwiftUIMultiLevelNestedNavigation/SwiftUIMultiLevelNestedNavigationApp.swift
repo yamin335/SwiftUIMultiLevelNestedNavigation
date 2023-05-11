115aa833f6e7616c6da47d7de60939a7736da17b
Yamin <myamin335@gmail.com>
2023-05-11 13:01:59 +0200

    Initial Commit

---
+++ SwiftUIMultiLevelNestedNavigation/SwiftUIMultiLevelNestedNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMultiLevelNestedNavigationApp.swift
+//  SwiftUIMultiLevelNestedNavigation
+//
+//  Created by Md. Yamin on 11.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMultiLevelNestedNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
